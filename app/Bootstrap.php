ed845f21235a24d7e98d663e3aad031f49f5560d
Kamil Dabrowski <kamildabrowski@users.noreply.github.com>
2024-11-17 14:59:25 +0100

    Fixed Path
    App/ in /app/
    * in /src/

---
+++ app/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php namespace app;
+
+
+class Bootstrap {
+    public static function run() {
+
+        // HERE ALL YOUR CODES
+
+        // Ex. $routes = new \App\Routes();
+        // Ex. $controller = new \App\Controller($routes);
+        // Ex. $view = new \App\View($controller);
+        // echo $view;
+
+
+        echo 'works';
+    }
+}
